cffe4a36cd1fada22fc7a57ade0e6da290579761
Art Eschenlauer <esch0041@umn.edu>
2020-03-21 19:30:54 -0500

    need path to delete temporary executable

--- examples/example_stdin.cmd
+++ examples/example_stdin.cmd
@@ -1,7 +1,8 @@
 @ set ERRORLEVEL=& setlocal & (echo off) & set SENTINEL=33780A5A-52A9-40B6-A46D-2F0619E8B0E5& :: NOT_ICON
 @ findstr /v "NOT_ICON" "%~dpnx0" | "%~dp0\..\icont.cmd" -o "%~dp0\%SENTINEL%.exe" -v 0 -   & :: NOT_ICON
 @ "%~dp0\%SENTINEL%.exe" %*                                                                 & :: NOT_ICON
-@ del %SENTINEL%.exe & exit /b %ERRORLEVEL%                                                 & :: NOT_ICON
+@ del "%~dp0\%SENTINEL%.exe" & exit /b %ERRORLEVEL%                                         & :: NOT_ICON
+@ :: Note that you will likely want to make the SENTINEL unique for each script - sorry       :: NOT_ICON
 
 # world.icn is the Icon program to be translated and run
 
