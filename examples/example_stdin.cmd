4492264b66de11055d48610038714d5a1cad20f4
Art Eschenlauer <esch0041@umn.edu>
2020-03-24 22:41:20 -0500

    simplified shebang

--- examples/example_stdin.cmd
+++ examples/example_stdin.cmd
@@ -1,12 +1,13 @@
-@ set ERRORLEVEL=& setlocal & (echo off) & set SENTINEL=33780A5A-52A9-40B6-A46D-2F0619E8B0E5& :: NOT_ICON
-@ findstr /v "NOT_ICON" "%~dpnx0" | "%~dp0\..\icont.cmd" -o "%~dp0\%SENTINEL%.exe" -v 0 -   & :: NOT_ICON
-@ "%~dp0\%SENTINEL%.exe" %*                                                                 & :: NOT_ICON
-@ del "%~dp0\%SENTINEL%.exe" & exit /b %ERRORLEVEL%                                         & :: NOT_ICON
-@ :: Note that you will likely want to make the SENTINEL unique for each script - sorry       :: NOT_ICON
+@REM NOT_ICON vim: sw=2 ts=2 et ai syntax=icon :
+@ set ERRORLEVEL=& setlocal & (echo off)                                           & :: NOT_ICON
+@ findstr /v "NOT_ICON" "%~dpnx0" | "%~dp0\..\icont.cmd" -o "%~dp0%~n0.exe" -v 0 - & :: NOT_ICON
+@ "%~dp0%~n0.exe" %*                                                               & :: NOT_ICON
+@ del "%~dp0\%~n0.exe" & exit /b %ERRORLEVEL%                                      & :: NOT_ICON
 
 # world.icn is the Icon program to be translated and run
 
 procedure main(args)
+  write(&progname)
   every write(!args)
   write(&version)
   every write(&features)
