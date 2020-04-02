46a9aa8970a4723254ad22f2d1e92871e0f75ce6
Art Eschenlauer <esch0041@umn.edu>
2020-04-02 08:25:42 -0500

    fix bad name for output file

--- examples/#!icon.cmd
+++ examples/#!icon.cmd
@@ -1,1 +1,1 @@
-@cmd /c ^"set IPATH=%~dp0..\IPL^& type "%~dpnx1" ^| call "%~dp0\..\icont_nopath.cmd" -o "%~dp0%~n0" -v 0 - ^& if exist "%~dp0%~n0.bat" ( "%~dp0\..\nticonx.exe" "%~dp0%~n0.bat" %* ^& del "%~dp0%~n0.bat" ) else (echo translation FAILED) ^"
+@cmd /c ^"set IPATH=%~dp0..\IPL^& type "%~dpnx1" ^| call "%~dp0\..\icont_nopath.cmd" -o "%~dp0%~n1" -v 0 - ^& if exist "%~dp0%~n1.bat" ( "%~dp0\..\nticonx.exe" "%~dp0%~n1.bat" %* ^& del "%~dp0%~n1.bat" ) else (echo translation FAILED) ^"
